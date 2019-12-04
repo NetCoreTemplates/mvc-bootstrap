47d558ead7e6d34786dbcc285178e318108dd8b8
Demis Bellot <demis.bellot@gmail.com>
2019-12-03 23:38:55 -0500

    Upgrade to .NET Core 3.1 LTS

--- MyApp/Startup.cs
+++ MyApp/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Funq;
@@ -15,16 +16,14 @@ namespace MyApp
 {
     public class Startup : ModularStartup
     {
-        public Startup(IConfiguration configuration) : base(configuration){}
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public new void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
