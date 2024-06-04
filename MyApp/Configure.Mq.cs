110c8f95f973f3af32f58694ad8e76056ea3c4ee
Demis Bellot <demis.bellot@gmail.com>
2024-06-04 17:34:17 +0800

    Add  CommandsFeature

--- MyApp/Configure.Mq.cs
+++ MyApp/Configure.Mq.cs
@@ -24,6 +24,7 @@ public class ConfigureMq : IHostingStartup
                 services.AddSingleton(smtpConfig);
             }
             services.AddSingleton<IMessageService>(c => new BackgroundMqService());
+            services.AddPlugin(new CommandsFeature());
         })
         .ConfigureAppHost(afterAppHostInit: appHost => {
             var mqService = appHost.Resolve<IMessageService>();
